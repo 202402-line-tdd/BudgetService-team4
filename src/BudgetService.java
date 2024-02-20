651fb5a4ecb945535be23eec174f78380ff8b3cb
Joey Chen <jay.hatelove@gmail.com>
2024-02-20 20:18:05 +0800

    move condition to loop

--- src/BudgetService.java
+++ src/BudgetService.java
@@ -1,8 +1,5 @@
-import java.time.Instant;
 import java.time.LocalDate;
-import java.time.Year;
 import java.time.YearMonth;
-import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -22,41 +19,35 @@ public class BudgetService {
         }
 
         Map<YearMonth, Double> budgetMap = budgetRepo.getAll().stream()
-                                                     .collect(Collectors.toMap(b -> YearMonth.parse(b.yearMonth, DateTimeFormatter.ofPattern("yyyyMM")), b -> b.amount));
+                .collect(Collectors.toMap(b -> YearMonth.parse(b.yearMonth, DateTimeFormatter.ofPattern("yyyyMM")), b -> b.amount));
 
         if (YearMonth.from(start).equals(YearMonth.from(end))) {
             Double localAmount = budgetMap.get(YearMonth.from(end));
             localAmount = localAmount == null ? 0 : localAmount;
             int days = end.getDayOfMonth() - start.getDayOfMonth() + 1;
-            return (localAmount/ start.lengthOfMonth()) * days;
+            return (localAmount / start.lengthOfMonth()) * days;
         }
         Double result = 0.0;
         LocalDate current = LocalDate.of(start.getYear(), start.getMonth(), 1);
-        while (true) {
-            if (current.isAfter(end)) {
-                break;
-            }
+        while (!current.isAfter(end)) {
             YearMonth currentYearMonth = YearMonth.from(current);
 
             if (budgetMap.containsKey(currentYearMonth)) {
                 Double amonut = budgetMap.get(YearMonth.from(current));
 
                 if (currentYearMonth.equals(YearMonth.from(start))) {
-                    int daysOfMonth =  start.lengthOfMonth() - start.getDayOfMonth() + 1;
-                    result += (amonut / start.lengthOfMonth())* daysOfMonth;
+                    int daysOfMonth = start.lengthOfMonth() - start.getDayOfMonth() + 1;
+                    result += (amonut / start.lengthOfMonth()) * daysOfMonth;
                 } else if (currentYearMonth.equals(YearMonth.from(end))) {
                     int daysOfMonth = end.getDayOfMonth();
-                    result += (amonut / end.lengthOfMonth())*daysOfMonth;
+                    result += (amonut / end.lengthOfMonth()) * daysOfMonth;
                 } else {
                     result += amonut;
                 }
             }
             current = current.plusMonths(1);
-
         }
 
         return result;
     }
-
-
 }
--- src/BudgetServiceTest.java
+++ src/BudgetServiceTest.java
@@ -1,10 +1,10 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 //import org.mockito.Mockito;
 
 public class BudgetServiceTest {
@@ -65,6 +65,4 @@ public class BudgetServiceTest {
                 )
         );
     }
-
-
 }
