6699e6bda17507d1c3f6296b89266190aa9713ae
Joey Chen <jay.hatelove@gmail.com>
2024-02-20 20:43:24 +0800

    introduce temp variables: firstDay and lastDay

--- src/Period.java
+++ src/Period.java
@@ -4,8 +4,10 @@ import static java.time.temporal.ChronoUnit.DAYS;
 
 public record Period(LocalDate start, LocalDate end) {
     long getOverlappingDays(Budget budget) {
-        final LocalDate overlappingStart = start.isAfter(budget.firstDay()) ? start : budget.firstDay();
-        final LocalDate overlappingEnd = end.isBefore(budget.lastDay()) ? end : budget.lastDay();
+        LocalDate firstDay = budget.firstDay();
+        LocalDate lastDay = budget.lastDay();
+        final LocalDate overlappingStart = start.isAfter(firstDay) ? start : firstDay;
+        final LocalDate overlappingEnd = end.isBefore(lastDay) ? end : lastDay;
         return DAYS.between(overlappingStart, overlappingEnd) + 1;
     }
 }
