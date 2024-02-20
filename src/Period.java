31395fa20f114537ed0615e0bcfdccf463a5fc18
Joey Chen <jay.hatelove@gmail.com>
2024-02-20 20:44:39 +0800

    inline temp variable: firstDay and lastDay

--- src/Period.java
+++ src/Period.java
@@ -5,10 +5,8 @@ import static java.time.temporal.ChronoUnit.DAYS;
 public record Period(LocalDate start, LocalDate end) {
     long getOverlappingDays(Budget budget) {
         Period another = new Period(budget.firstDay(), budget.lastDay());
-        LocalDate firstDay = another.start;
-        LocalDate lastDay = another.end;
-        final LocalDate overlappingStart = start.isAfter(firstDay) ? start : firstDay;
-        final LocalDate overlappingEnd = end.isBefore(lastDay) ? end : lastDay;
+        final LocalDate overlappingStart = start.isAfter(another.start) ? start : another.start;
+        final LocalDate overlappingEnd = end.isBefore(another.end) ? end : another.end;
         return DAYS.between(overlappingStart, overlappingEnd) + 1;
     }
 }
