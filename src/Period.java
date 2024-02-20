24cf9d798d2d565d41671ba232cc7ca3f59c5ebb
Joey Chen <jay.hatelove@gmail.com>
2024-02-20 20:44:19 +0800

    use another period, instead of budget

--- src/Period.java
+++ src/Period.java
@@ -5,8 +5,8 @@ import static java.time.temporal.ChronoUnit.DAYS;
 public record Period(LocalDate start, LocalDate end) {
     long getOverlappingDays(Budget budget) {
         Period another = new Period(budget.firstDay(), budget.lastDay());
-        LocalDate firstDay = budget.firstDay();
-        LocalDate lastDay = budget.lastDay();
+        LocalDate firstDay = another.start;
+        LocalDate lastDay = another.end;
         final LocalDate overlappingStart = start.isAfter(firstDay) ? start : firstDay;
         final LocalDate overlappingEnd = end.isBefore(lastDay) ? end : lastDay;
         return DAYS.between(overlappingStart, overlappingEnd) + 1;
