07702644ce641605c967d3ef17b41ac570328932
Joey Chen <jay.hatelove@gmail.com>
2024-02-20 20:49:38 +0800

    clean up and inline variable: budgets

--- src/BudgetService.java
+++ src/BudgetService.java
@@ -1,5 +1,4 @@
 import java.time.LocalDate;
-import java.util.List;
 
 public class BudgetService {
     private final BudgetRepo budgetRepo;
@@ -15,19 +14,9 @@ public class BudgetService {
             return 0.00;
         }
 
-        final List<Budget> budgets = budgetRepo.getAll();
-
-//        if (YearMonth.from(start).equals(YearMonth.from(end))) {
-//            final Map<YearMonth, Double> budgetMap = budgets.stream()
-//                    .collect(Collectors.toMap(b -> YearMonth.parse(b.yearMonth, DateTimeFormatter.ofPattern("yyyyMM")), b -> b.amount));
-//            Double localAmount = budgetMap.get(YearMonth.from(end));
-//            localAmount = localAmount == null ? 0 : localAmount;
-//            final int days = end.getDayOfMonth() - start.getDayOfMonth() + 1;
-//            return (localAmount / start.lengthOfMonth()) * days;
-//        }
         double result = 0.0;
         final Period period = new Period(start, end);
-        for (Budget budget : budgets) {
+        for (Budget budget : budgetRepo.getAll()) {
             result += budget.overlappingAmount(period);
         }
 
