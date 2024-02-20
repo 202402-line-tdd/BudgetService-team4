c7d45fde2ee41aa8fdb88fb2be3a715d42a9bf27
Joey Chen <jay.hatelove@gmail.com>
2024-02-20 20:42:52 +0800

    remove useless if/else block

--- src/Period.java
+++ src/Period.java
@@ -1,5 +1,4 @@
 import java.time.LocalDate;
-import java.time.YearMonth;
 
 import static java.time.temporal.ChronoUnit.DAYS;
 
@@ -7,16 +6,6 @@ public record Period(LocalDate start, LocalDate end) {
     long getOverlappingDays(Budget budget) {
         final LocalDate overlappingStart = start.isAfter(budget.firstDay()) ? start : budget.firstDay();
         final LocalDate overlappingEnd = end.isBefore(budget.lastDay()) ? end : budget.lastDay();
-        if (budget.getYearMonth().equals(YearMonth.from(start()))) {
-//            overlappingStart = start();
-//            overlappingEnd = budget.lastDay();
-        } else if (budget.getYearMonth().equals(YearMonth.from(end()))) {
-//            overlappingStart = budget.firstDay();
-//            overlappingEnd = end();
-        } else {
-//            overlappingStart = budget.firstDay();
-//            overlappingEnd = budget.lastDay();
-        }
         return DAYS.between(overlappingStart, overlappingEnd) + 1;
     }
 }
