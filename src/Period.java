e661ef77dd5674ff7882c594a9b0f61780468b65
Joey Chen <jay.hatelove@gmail.com>
2024-02-20 20:42:08 +0800

    use Period domain logic for overlappingStart

--- src/Period.java
+++ src/Period.java
@@ -5,16 +5,16 @@ import static java.time.temporal.ChronoUnit.DAYS;
 
 public record Period(LocalDate start, LocalDate end) {
     long getOverlappingDays(Budget budget) {
-        final LocalDate overlappingStart;
+        final LocalDate overlappingStart = start.isAfter(budget.firstDay()) ? start : budget.firstDay();
         final LocalDate overlappingEnd;
         if (budget.getYearMonth().equals(YearMonth.from(start()))) {
-            overlappingStart = start();
+//            overlappingStart = start();
             overlappingEnd = budget.lastDay();
         } else if (budget.getYearMonth().equals(YearMonth.from(end()))) {
-            overlappingStart = budget.firstDay();
+//            overlappingStart = budget.firstDay();
             overlappingEnd = end();
         } else {
-            overlappingStart = budget.firstDay();
+//            overlappingStart = budget.firstDay();
             overlappingEnd = budget.lastDay();
         }
         return DAYS.between(overlappingStart, overlappingEnd) + 1;
